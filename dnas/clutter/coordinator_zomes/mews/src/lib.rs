6de158764f6de6c8e379a7032570c1b76dd75fbe
Harlan Wood <harlan.wood@sharespost.com>
2023-04-01 22:50:49 -1000

    Iterate on Recommended feed DNA func

--- dnas/clutter/coordinator_zomes/mews/src/lib.rs
+++ dnas/clutter/coordinator_zomes/mews/src/lib.rs
@@ -1,7 +1,8 @@
 use hdk::prelude::*;
 use mews_integrity::*;
 use regex::Regex;
-use trust_atom_types::{QueryMineInput, TrustAtomInput};
+use thiserror::Error;
+use trust_atom_types::{QueryMineInput, TrustAtom};
 
 fn get_my_mews_base(base_type: &str, ensure: bool) -> ExternResult<EntryHash> {
     let me: AgentPubKey = agent_info()?.agent_latest_pubkey;
@@ -270,11 +271,11 @@ pub fn mews_feed(_options: FeedOptions) -> ExternResult<Vec<FeedMew>> {
 }
 
 #[hdk_extern]
-pub fn recommended() -> ExternResult<Vec<FeedMew>> {
+pub fn recommended(_: ()) -> ExternResult<Vec<FeedMew>> {
     let mut feed = Vec::new();
     let me = agent_info()?.agent_latest_pubkey;
     // TODO figure out how to manage what call returns
-    let mut atoms: Vec<TrustAtom> = call(
+    let response = call(
         CallTargetCell::Local,
         ZomeName::from("trust_atom"),
         FunctionName::from("query_mine"),
@@ -287,48 +288,153 @@ pub fn recommended() -> ExternResult<Vec<FeedMew>> {
         },
     )?;
 
+    let result_io = match response {
+        ZomeCallResponse::Ok(bytes) => Ok(bytes),
+        ZomeCallResponse::Unauthorized(zome_call_auth, cell_id, zome, func, agent) => Err(
+            wasm_error!("ZomeCallResponse::Unauthorized: zome_call_auth: {:?}, cell_id: {:?}, zome: {:?}, func: {:?}, agent: {:?}", zome_call_auth, cell_id, zome, func, agent))
+        ,
+        ZomeCallResponse::NetworkError(message) => Err(wasm_error!("ZomeCallResponse::NetworkError: {}", message)),
+        ZomeCallResponse::CountersigningSession(message) => {
+            Err(wasm_error!("ZomeCallResponse::CountersigningSession: {}", message))
+        }
+    }?;
+
+    let atoms: Vec<TrustAtom> = result_io.decode().map_err(|e|
+            wasm_error!(
+                "Could not decode response to local zome call (zome: {}) (function: {}) (response bytes: {}): {}",
+                "trust_atom",  // TODO make this a util func, pass in zome and func name
+                "query_mine",
+                result_io.as_bytes().len(),
+                e
+            )
+        )?;
+
     // filter for those TrustAtoms above a weight threshold (nominally >= 0)
     feed = atoms
         .into_iter()
-        .filter_map( |atom|
-            match(atom.value) {
-                Some(value_string) => {
-                    let value_decimal = Decimal::from_str(value_str.as_str());
-                    match (value_decimal) {
-                        Ok(value_decimal) => {
-                            if value_decimal >= Decimal::ZERO{
-                                Some(value_string)
-                            } else {
-                                None
-                            }
-                        },
-                        Err(error) => Err(wasm_error!(
-                            "Unexpected value: `{}`.  Error: `{}`",
-                            value_string,
-                            error
-                        )),
+        .filter_map(|atom| match atom.value {
+            Some(value_string) => {
+                let value_float: Result<f32, _> = value_string.parse();
+                match value_float {
+                    Ok(value_float) => {
+                        if value_float >= 0f32 {
+                            Some(atom)
+                        } else {
+                            None
+                        }
                     }
-                },
-                None => Some(None), // trust atoms with no value typically mean "yup" without a specific "percent/weight"
+                    _ => None,
+                }
             }
-        ).flat_map( |atom|
-            followed_author = atom.target ;
-            links_to_posts = get_links(followed_author, LinkTypes::Mew); // TODO we want to get only links for this hashtag: atom.content
+            None => None,
+        })
+        .filter_map(|atom| {
+            let followed_author = atom.target_hash;
+            get_links(followed_author, LinkTypes::Mew, None)?
+            // TODO we want to get only links for this hashtag: atom.content                                                                                    // TODO should we get other types: mewmews, quotes, etc
+        })
+        .flat_map(|links_to_posts| {
             // TODO should we get other types: mewmews, quotes, etc
-            links_to_posts.map(|link_to_post|
+            links_to_posts.into_iter().map(|link_to_post| {
                 get_feed_mew_and_context(ActionHash::from(link_to_post.target).into())
-            )
+            })
 
             // TODO: also get the creation date, either from the entry or the link itself
+        })
+        .collect();
+    feed.sort_by(|a, b| b.action.timestamp().cmp(&a.action.timestamp()));
+    // TODO *maybe* also  order these posts according to some combination of: weight of tags and recency
+    // (don't want to see very old highly weighted at top)
+    // *buttttt* maybe just chron is good!  if only showing last time chunk, or (later) mews since last visit
+    // it really depends if that last chunk is 100 mews (chron fine) or 10,000 mews (then weight of tags is awesome)
 
-        ).collect();
-        feed.sort_by(|a, b| b.action.timestamp().cmp(&a.action.timestamp()));
-        // TODO *maybe* also  order these posts according to some combination of: weight of tags and recency
-        // (don't want to see very old highly weighted at top)
-        // *buttttt* maybe just chron is good!  if only showing last time chunk, or (later) mews since last visit
-        // it really depends if that last chunk is 100 mews (chron fine) or 10,000 mews (then weight of tags is awesome)
+    Ok(feed)
+}
 
-        Ok(feed)
+// TODO move these utils to elsewhere:
+
+pub fn call_local_zome<T, A>(zome: &str, func: &str, input: A) -> AppResult<T>
+where
+    T: serde::de::DeserializeOwned + std::fmt::Debug,
+    A: serde::Serialize + std::fmt::Debug,
+{
+    let response = call(CallTargetCell::Local, zome, func.into(), None, input)?;
+
+    Ok(interpret_zome_response(response)?)
+}
+
+fn interpret_zome_response<T>(response: ZomeCallResponse) -> AppResult<T>
+where
+    T: serde::de::DeserializeOwned + std::fmt::Debug,
+{
+    let result_io = zome_call_response_as_result(response)?;
+    // let essence: DevHubResponse<T> = result_io.decode().map_err(|e| {
+    //     AppError::DeserializeError(format!(
+    //         "Could not decode Essence response ({} bytes): {}",
+    //         result_io.as_bytes().len(),
+    //         e
+    //     ))
+    // })?;
+
+    // Ok(essence.as_result()?)
+}
+
+fn zome_call_response_as_result(response: ZomeCallResponse) -> AppResult<zome_io::ExternIO> {
+    Ok(match response {
+        ZomeCallResponse::Ok(bytes) => Ok(bytes),
+        ZomeCallResponse::Unauthorized(zome_call_auth, cell_id, zome, func, agent) => Err(
+            AppError::UnauthorizedError(zome_call_auth, cell_id, zome, func, agent),
+        ),
+        ZomeCallResponse::NetworkError(message) => Err(AppError::NetworkError(message)),
+        ZomeCallResponse::CountersigningSession(message) => {
+            Err(AppError::CountersigningSessionError(message))
+        }
+    }?)
+}
+
+#[derive(Debug, Error)]
+pub enum AppError {
+    #[error("Unexpected state: {0}")]
+    UnexpectedStateError(String),
+
+    #[error(
+        "Agent '{4}' does not have the capability to call {1}::{2}->{3}( [args] ) because; {0}"
+    )]
+    UnauthorizedError(
+        ZomeCallAuthorization,
+        CellId,
+        ZomeName,
+        FunctionName,
+        AgentPubKey,
+    ),
+
+    #[error("{0}")]
+    NetworkError(String),
+
+    #[error("{0}")]
+    CountersigningSessionError(String),
+
+    #[error("{0}")]
+    DeserializeError(String),
+}
+
+pub type AppResult<T> = Result<T, ErrorKinds>;
+
+#[derive(Debug, Error)]
+pub enum ErrorKinds {
+    #[error(transparent)]
+    AppError(AppError),
+    // #[error(transparent)]
+    // UserError(UserError),
+
+    // #[error(transparent)]
+    // DnaUtilsError(UtilsError),
+
+    // #[error(transparent)]
+    // FailureResponseError(EssenceError),
+
+    // #[error(transparent)]
+    // HDKError(WasmError), // catch all for unhandled errors
 }
 
 // *** Liking ***
@@ -511,7 +617,7 @@ pub fn parse_mew_text(mew_content: MewContent, mew_hash: ActionHash) -> ExternRe
         create_mew_tag_links("hashtags", mat.as_str(), mew_hash.clone())?;
     }
     for mat in cashtag_regex.find_iter(&mew_content.text.clone()) {
-       create_mew_tag_links("cashtags", mat.as_str(), mew_hash.clone())?;
+        create_mew_tag_links("cashtags", mat.as_str(), mew_hash.clone())?;
     }
     if let Some(links) = mew_content.links {
         for link in links {
@@ -521,7 +627,7 @@ pub fn parse_mew_text(mew_content: MewContent, mew_hash: ActionHash) -> ExternRe
                     let path_hash = path.path_entry_hash()?;
                     let _link_ah = create_link(path_hash, mew_hash.clone(), LinkTypes::Tag, ())?;
                 }
-                _ => ()
+                _ => (),
             }
         }
     }
@@ -546,10 +652,13 @@ fn search_tags(path_stem: String, content: String) -> ExternResult<Vec<String>> 
 
     let tags: Vec<String> = links
         .into_iter()
-        .map(|link|
-            String::from_utf8(link.tag.into_inner()).map_err(|_| wasm_error!(WasmErrorInner::Guest("Failed to convert link tag to string".into())))
-
-        )
+        .map(|link| {
+            String::from_utf8(link.tag.into_inner()).map_err(|_| {
+                wasm_error!(WasmErrorInner::Guest(
+                    "Failed to convert link tag to string".into()
+                ))
+            })
+        })
         .filter_map(Result::ok)
         .collect();
 
@@ -565,14 +674,19 @@ fn create_mew_tag_links(path_stem: &str, content: &str, mew_hash: ActionHash) ->
 
     // Create Path for sliced hashtag (first 3 characters) under hashtags_search.myt
     // Link from Path hashtags.myt -> hashtags.mytag Path
-    let word: String =  content.chars().skip(1).collect();
+    let word: String = content.chars().skip(1).collect();
     let word_lowercase = word.to_lowercase();
     let prefix: String = word_lowercase.chars().take(3).collect();
 
     let search_path = Path::from(format!("search_{}.{}", path_stem, prefix));
     let search_path_hash = search_path.path_entry_hash()?;
     search_path.typed(LinkTypes::TagPrefix)?.ensure()?;
-    let _link_search_tag = create_link(search_path_hash, path_hash.clone(), LinkTypes::TagPrefix, word.as_bytes().to_vec())?;
+    let _link_search_tag = create_link(
+        search_path_hash,
+        path_hash.clone(),
+        LinkTypes::TagPrefix,
+        word.as_bytes().to_vec(),
+    )?;
 
     Ok(())
 }
